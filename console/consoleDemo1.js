b6b71529442d1bd858441df7875a7f780c9d367b
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-20 17:27:03 +0530

    console

---
+++ console/consoleDemo1.js
@@ -0,0 +1,9 @@
+console.log("file loaded")
+console.warn("warning...")
+console.error("error...")
+
+var no1 = 100
+var no2 = 200
+
+console.log("no1 = "+no1+"no2 = "+no2)
+console.log(`no1 = ${no1} no2 = ${no2}`)
