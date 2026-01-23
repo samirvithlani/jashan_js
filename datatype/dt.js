6c0b112418fa70c6eb22dccc5a848b36205c1d7e
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-23 14:15:39 +0530

    json

---
+++ datatype/dt.js
@@ -0,0 +1,15 @@
+var data = "java"
+console.log(data)
+console.log(typeof(data)) //string
+
+var no1 = 123.2
+console.log(no1,typeof(no1)) //number
+
+var isActive = false
+console.log(isActive,typeof(isActive)) //boolean
+
+var x;
+console.log(x,typeof(x)) //undefined
+
+var x1 =null
+console.log(x1,typeof(x1)) //object
---
+++ json/json1.js
@@ -0,0 +1,34 @@
+var carName = "bmw"
+var carColor = "black"
+var carPrice = 123456
+var engine = "v8"
+//...
+//...
+
+//json javascript object notaiton..
+
+var car ={
+    name:"audi",
+    price:23456,
+    // color:"red",
+    // engine:"v8",
+    // cap:5
+    ext:{
+        color:"red",
+        headlight:"white"
+    },
+    int:{
+        type:"wooden",
+        color:"black"
+    }
+}
+console.log(car)
+console.log(car.name)
+//console.log(car.cap)
+console.log(car.price)
+console.log(car.int.color)
+console.log(car.ext.color)
+
+
+//student -->
+//product -->
