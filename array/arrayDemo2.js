7f2946bb9f35075486b4dcac13a3037b7d9d82d9
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-04 16:54:55 +0530

    splice

---
+++ array/arrayDemo2.js
@@ -0,0 +1,7 @@
+var users = ["raj","parth","samir","amit","sumit","kunal"]
+console.log(users)
+//users.splice(1,1) //1st index delete 1 element
+//users.splice(2,3) //2nd index delete 3 element
+//users.splice(1,0,"kunal") //1st index delete 1 element
+users.splice(2,1,"kunal","ajay","neha") //1st index delete 1 element
+console.log(users)
