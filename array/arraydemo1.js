9acd9dcecd95c0c858426ea7a330cba4348c413d
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-04 16:39:50 +0530

    array

--- array/arraydemo1.js
+++ array/arraydemo1.js
@@ -1,2 +1,23 @@
 var users = ["raj","parth","samir","amit","sumit","kunal"]
 console.log(users)
+
+console.log(users[0])
+console.log(users[1])
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i])
+// }
+
+
+//manuplate
+users.push("ajay")
+console.log(users)
+var removedelm = users.pop()
+console.log("removing...",removedelm)
+console.log(users)
+
+users.unshift("priyanka")
+console.log(users)
+var removedelm = users.shift()
+console.log("removing...",removedelm)
+console.log(users)
