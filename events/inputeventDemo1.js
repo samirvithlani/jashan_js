d5c3f9099ea0ad790d739d44e2e72ebf5bb86e32
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-25 16:24:32 +0530

    inputs

---
+++ events/inputeventDemo1.js
@@ -0,0 +1,18 @@
+const getData =()=>{
+    const text = document.getElementById("txt")
+    console.log(text.value)
+}
+const getData1 =()=>{
+    const text = document.getElementById("txt1")
+    console.log(text.value)
+}
+const getData2 =()=>{
+    const text = document.getElementById("txt2")
+    console.log(text.value)
+}
+
+const checkEmail=()=>{
+
+    const  email = document.getElementById("email")
+    console.log(email.value)
+}
