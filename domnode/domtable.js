39dc6a0b2610c0a94a365ee116f8a9db67dc05e7
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-27 14:59:26 +0530

    dynamic table

--- domnode/domtable.js
+++ domnode/domtable.js
@@ -5,10 +5,30 @@ const tableBody = document.createElement("tbody")
 tableHader.innerHTML=""
 const mainTr = document.createElement("tr")
 table.appendChild(tableHader)
-for(i=1;i<=10;i++){
+table.appendChild(tableBody)
+const th1 = document.createElement("th")
+th1.innerHTML="X"
+mainTr.appendChild(th1)
+for(i=1;i<=15;i++){
     const mainTh = document.createElement("th")
     mainTh.innerHTML = i
     mainTr.appendChild(mainTh)
 }
 tableHader.appendChild(mainTr)
 
+
+
+for(let row =1;row<=15;row++){
+    const tr = document.createElement("tr")
+    const sideHeader =document.createElement("th")
+    sideHeader.innerHTML = row
+    tr.appendChild(sideHeader)
+
+    //mul
+    for(let col=1;col<=15;col++){
+        const td = document.createElement("td")
+        td.innerHTML = row * col;
+        tr.appendChild(td)
+    }
+    tableBody.appendChild(tr)
+}
